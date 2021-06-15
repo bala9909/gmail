1818a9f7e0d2a7dbdb274c6aa98df65dbcdc96a4
bala9909 <balajojiupputholla2013@gmail.com>
2021-06-15 22:45:13 +0530

    back end processing

--- registration/js/registration.js
+++ registration/js/registration.js
@@ -1,1 +1,43 @@
-alert("ndkikdi");
+function registerUser() {
+    const response = new XMLHttpRequest();
+
+    const json = JSON.stringify({
+        firstName: document.getElementById('fname').value,
+        lastName: document.getElementById('lname').value,
+        userName: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+       
+
+    });
+
+    response.open("POST", 'http://localhost:8080/users')
+    response.setRequestHeader('Content-Type', 'application/json');
+
+    response.send(json);
+
+    response.onload = (e) => {
+        alert(response.response);
+    }
+}
+
+
+/*function deleteEmpRecord() {
+	
+	int empid = 100;
+		
+	RestAssured.baseURI = "http://localhost:8080/users";
+	RequestSpecification request = RestAssured.given();	
+		
+	// Add a header stating the Request body is a JSON
+	request.header("Content-Type", "application/json");	
+	
+       // Delete the request and check the response
+	Response response = request.delete("/delete/"+ empid);		
+		
+	int statusCode = response.getStatusCode();
+	System.out.println(response.asString());
+	Assert.assertEquals(statusCode, 200);
+		
+	String jsonString =response.asString();
+	Assert.assertEquals(jsonString.contains("successfully! deleted Records"), true);
+	}*/
