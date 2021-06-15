7a7128b8bd1dc14fe6f3bd2c4f451417d7a8fc5a
suputhul <suresh.uputhula@asml.com>
2021-06-15 20:48:25 +0200

    ref(users): comments and clean up

--- registration/js/registration.js
+++ registration/js/registration.js
@@ -1,9 +1,11 @@
+/* Reading URL query params*/
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 if (params && params.id) {
     getUserFromService(params.id);
 }
 
+/* Fetches user data with userID from the service*/
 function getUserFromService(userID) {
     let request = new XMLHttpRequest();
     request.open("GET", "http://localhost:8080/users/" + userID);
@@ -11,20 +13,22 @@ function getUserFromService(userID) {
     request.onload = () => {
         if (request.status === 200) {
             console.log(JSON.parse(request.response));
-            addDataToHtmlForm(JSON.parse(request.response))
+            populateDataToHtmlForm(JSON.parse(request.response))
         } else {
             console.log("error");
         }
     };
 }
 
-function addDataToHtmlForm(userData) {
+/* Populates the user data to html registration form*/
+function populateDataToHtmlForm(userData) {
     document.getElementById('fname').value = userData.firstName;
     document.getElementById('lname').value = userData.lastName;
     document.getElementById("username").value = userData.userName;
     document.getElementById("password").value = userData.password;
 }
 
+/* Fetches the values entered in the registration from and posts that data to backend service */
 function registerUser() {
     const response = new XMLHttpRequest();
 
@@ -33,38 +37,14 @@ function registerUser() {
         lastName: document.getElementById('lname').value,
         userName: document.getElementById("username").value,
         password: document.getElementById("password").value,
-
-
     });
 
     response.open("POST", 'http://localhost:8080/users');
     response.setRequestHeader('Content-Type', 'application/json');
-
     response.send(json);
-
     response.onload = (e) => {
         alert(response.response);
     }
 }
 
 
-/*function deleteEmpRecord() {
-	
-	int empid = 100;
-		
-	RestAssured.baseURI = "http://localhost:8080/users";
-	RequestSpecification request = RestAssured.given();	
-		
-	// Add a header stating the Request body is a JSON
-	request.header("Content-Type", "application/json");	
-	
-       // Delete the request and check the response
-	Response response = request.delete("/delete/"+ empid);		
-		
-	int statusCode = response.getStatusCode();
-	System.out.println(response.asString());
-	Assert.assertEquals(statusCode, 200);
-		
-	String jsonString =response.asString();
-	Assert.assertEquals(jsonString.contains("successfully! deleted Records"), true);
-	}*/
--- users/js/users.js
+++ users/js/users.js
@@ -1,3 +1,4 @@
+/* Fetches user records from the service */
 let request = new XMLHttpRequest();
 request.open("GET", "http://localhost:8080/users");
 request.send();
@@ -11,6 +12,7 @@ request.onload = () => {
     }
 };
 
+/* Creates user rows from the user data and then appends rows to users table */
 function addDataToTable(userArray) {
     let tableElement = document.getElementById('userTable');
     userArray.forEach(user => {
@@ -25,7 +27,7 @@ function addDataToTable(userArray) {
         lastNameCell.innerText = user.lastName;
         let passwordCell = document.createElement("TD");
         passwordCell.innerText = user.password;
-
+        // Edit button creation
         let editButtonCell = document.createElement("TD");
         let editButton = document.createElement("BUTTON");
         editButton.innerText = 'Edit';
@@ -34,7 +36,7 @@ function addDataToTable(userArray) {
             editUser(user.id);
         });
         editButtonCell.appendChild(editButton);
-
+        // Delete button creation
         let deleteButtonCell = document.createElement("TD");
         let deleteButton = document.createElement("BUTTON");
         deleteButton.innerText = 'Delete';
@@ -43,8 +45,6 @@ function addDataToTable(userArray) {
             deleteUser(user.id);
         });
         deleteButtonCell.appendChild(deleteButton);
-
-
         tableRow.appendChild(userNameCell);
         tableRow.appendChild(firstNameCell);
         tableRow.appendChild(lastNameCell);
@@ -55,13 +55,13 @@ function addDataToTable(userArray) {
 
 }
 
+/* Navigates to the registration page with query param id */
 function editUser(userId) {
     alert('edit user' + userId);
     window.location = '../../registration/html/registration.html?id=' + userId;
-
-
 }
 
+/* Calls the delete service with the provided user id */
 function deleteUser(userId) {
     let request = new XMLHttpRequest();
     request.open("DELETE", "http://localhost:8080/users/" + userId);
@@ -76,6 +76,7 @@ function deleteUser(userId) {
     };
 }
 
+/*Deletes the specified user row from the HTML user table*/
 function deleteUserFromHtmlTable(userId) {
     let tableElement = document.getElementById('userTable');
     let deleteElement = document.getElementById(userId);
